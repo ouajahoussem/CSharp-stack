81ae723af1d93815bafb039f4e288d511d8d8a25
ouajahoussem <ouaja.houssem@gmail.com>
2024-03-11 17:38:11 +0100

    SCharp

---
+++ oopWithCsharp/core/gameDeveloper_1/Attack.cs
@@ -0,0 +1,22 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+public class Attack 
+{
+    public string Name {get;set;}
+    public int DamageAmount {get;set;}
+
+    public Attack (string n,int dmg)
+    {
+        Name = n;
+        DamageAmount= dmg;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Damage: {DamageAmount}");
+        Console.WriteLine("-------");
+
+    }
+}
---
+++ oopWithCsharp/core/gameDeveloper_1/Enemy.cs
@@ -0,0 +1,31 @@
+public class Enemy : Attack
+{
+    public string EnemyName;
+    protected int Health = 100;
+    public int _Health {get {return Health;}}
+
+
+
+    public Enemy(string name, string n, int dmg) : base(n, dmg)
+    {
+        EnemyName = name;
+    }
+    public virtual void showInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine($"Name: {EnemyName}");
+        Console.WriteLine($"Health: {_Health}");
+        // Console.WriteLine($"Attack Name: {Name}");
+        // Console.WriteLine($"Damage: {DamageAmount}");
+        // Console.WriteLine("-------");
+
+    }
+    public void performattack(Enemy target)
+    {
+        Console.WriteLine($"{EnemyName} performs {Name} on {target.EnemyName}   ");
+        target.Health -= DamageAmount;
+        Console.WriteLine($"{target.EnemyName} takes {DamageAmount} damage. {target.EnemyName}'s Health: {target.Health}");
+
+    }
+
+}
