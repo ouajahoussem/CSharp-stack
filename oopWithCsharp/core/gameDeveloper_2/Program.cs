ae2f31b3f5c420c207b7c44428dabfb2d7c27322
ouajahoussem <ouaja.houssem@gmail.com>
2024-03-19 02:35:07 +0100

    CSharp

---
+++ oopWithCsharp/core/gameDeveloper_2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Attack punch = new Attack("Punch", 5);
+Attack Throw = new Attack("Throw", 10);
+Attack fireball = new Attack("Fireball", 25);
+
+List<Attack> allAttacks = new List<Attack> { punch, Throw, fireball };
+
+
+Enemy brand = new Enemy("Brand", "fireball",25);
+Enemy sett = new Enemy("Sett", "Punch",5);
+Enemy  draven = new Enemy("Draven", "Throw",10);
+brand.performattack(draven);
+draven.performattack(brand);
+MeleeFighter voli = new MeleeFighter("Voli");
+
+
+voli.showInfo();
+
+List<Enemy> enemies = new List<Enemy>();
+{
+    enemies.Add(brand);
+    enemies.Add(sett);
+    enemies.Add(draven);
+}
+
+foreach (Enemy A in enemies)
+{
+    
+    Console.WriteLine($"{A}-{A.EnemyName}"  );
+    Console.WriteLine("Health: " + A.Health );
+    Console.WriteLine("Attack Name: " + A.Name );
+    Console.WriteLine("Damage: " + A.DamageAmount);
+    Console.WriteLine("********");
+    
+}
+voli.RageAttack(brand);
+voli.RageAttack(sett);
+
+
+// brand.showInfo();
+// sett.showInfo();
+
+// punch.ShowInfo();
+// Throw.ShowInfo();
+// fireball.ShowInfo();
+
+
+
+
+
+
+
