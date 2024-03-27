c969672aa2b6770a61b50adb1ab019f37ffe7b85
ouajahoussem <ouaja.houssem@gmail.com>
2024-03-28 00:47:04 +0100

    CSharp

---
+++ ASP_NET Core/practice/MVC_2/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    public static Student student;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("process")]
+    public IActionResult Process(Student NewStudent)
+    {
+        student = NewStudent;
+        
+        return RedirectToAction("Results");
+    }
+
+    [HttpGet("results")]
+    public IActionResult Results()
+
+    {
+        
+        return View(student);
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASP_NET Core/practice/MVC_2/DojoSurveyWithModel/Models/Student.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+namespace DojoSurveyWithModel.Models;
+
+public class Student
+{
+    public string Name{get; set;}
+    public string Location{get; set;}
+    public string FavoriteLanguage{get; set;}
+    public string Comment{get; set;}
+
+}
