7fd5b553bf6c3f5d06551c810d8361e0a8a5f107
ouajahoussem <ouaja.houssem@gmail.com>
2024-04-08 10:51:29 +0100

    CSharp

---
+++ ORMs/practice/FirstConnection/Models/MyContext.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+
+using Microsoft.EntityFrameworkCore;
+namespace FirstConnection.Models;
+
+public class MyContext : DbContext 
+{   
+    public MyContext(DbContextOptions options) : base(options) { }    
+    public DbSet<Pet> Pets { get; set; } 
+}
